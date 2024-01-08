f0c3f45b0da41539d7b3574a509886ea9cd64adf
Crowdbotics <engineering@crowdbotics.com>
2024-01-08 14:27:58 +0000

    Update store.

--- store/dockerchangetestAPI/api.js
+++ store/dockerchangetestAPI/api.js
@@ -14,6 +14,45 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return dockerchangetestAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_social_auth_apple_connect_create(payload) {
+  return dockerchangetestAPI.post(
+    `/modules/social-auth/apple/connect/`,
+    payload
+  )
+}
+function modules_social_auth_apple_login_create(payload) {
+  return dockerchangetestAPI.post(`/modules/social-auth/apple/login/`, payload)
+}
+function modules_social_auth_facebook_connect_create(payload) {
+  return dockerchangetestAPI.post(
+    `/modules/social-auth/facebook/connect/`,
+    payload
+  )
+}
+function modules_social_auth_facebook_login_create(payload) {
+  return dockerchangetestAPI.post(
+    `/modules/social-auth/facebook/login/`,
+    payload
+  )
+}
+function modules_social_auth_google_connect_create(payload) {
+  return dockerchangetestAPI.post(
+    `/modules/social-auth/google/connect/`,
+    payload
+  )
+}
+function modules_social_auth_google_login_create(payload) {
+  return dockerchangetestAPI.post(`/modules/social-auth/google/login/`, payload)
+}
+function modules_social_auth_socialaccounts_list(payload) {
+  return dockerchangetestAPI.get(`/modules/social-auth/socialaccounts/`)
+}
+function modules_social_auth_socialaccounts_disconnect_create(payload) {
+  return dockerchangetestAPI.post(
+    `/modules/social-auth/socialaccounts/${payload.id}/disconnect/`,
+    payload
+  )
+}
 function rest_auth_login_create(payload) {
   return dockerchangetestAPI.post(`/rest-auth/login/`, payload)
 }
@@ -57,6 +96,14 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_social_auth_apple_connect_create,
+  modules_social_auth_apple_login_create,
+  modules_social_auth_facebook_connect_create,
+  modules_social_auth_facebook_login_create,
+  modules_social_auth_google_connect_create,
+  modules_social_auth_google_login_create,
+  modules_social_auth_socialaccounts_list,
+  modules_social_auth_socialaccounts_disconnect_create,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
